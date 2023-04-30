5e12d4594b2ab0f4fdd3ab65874edde8c6ecdc42
emily <pauxcode@gmail.com>
2023-04-30 14:54:33 -0600

    Bug Fixed

--- ProcesoPrincipal.c
+++ ProcesoPrincipal.c
@@ -1,131 +1,97 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/shm.h>
 #include <unistd.h>
 
-void asignarValores(int *matriz,int filas, int columnas){
-    int i = 0; // Indicador auxiliar para llenar la matriz
-    // Fila 1
-    int f1= 1;
-    for(i=0; i<columnas; i++){
-        matriz[i*filas + 0] = f1; // Equivalente a matriz[0][i] 
+void assignMatrixValues(int *matrix, int rows, int columns){
+    int f1 = 1, f2 = 2, f3 = 3;
+
+    for(int i=0; i<columns; ++i){
+        matrix[i*rows + 0] = f1;
         f1 += 1;
-    }
 
-    // Fila 2
-    int f2= 1;
-    for(i=0; i<columnas; i++){
-        matriz[i*filas + 1] = f2; // Equivalente a matriz[1][i] 
+        matrix[i*rows + 1] = f2;
         f2 += 2;
-    }
 
-    // Fila 3
-    int f3= 2;
-    for(i=0; i<columnas; i++){
-        matriz[i*filas + 2] = f3; // Equivalente a matriz[2][i] 
+        matrix[i*rows + 2] = f3;
         f3 += 2;
     }
 
-    printf("Matriz creada correctamente! :)\n");
+    printf("Successfully created matrix! :)\n");
 }
 
-void leerMatriz(int *matriz, int filas, int columnas){
-    for(int i=0; i<filas; i++){
-        for(int j=0; j<columnas; j++){
-            printf("%d ", matriz[j*filas + i]); // Equivalente a matrix[][] 
+void assignResultsValues(int *resultsList, int rows){
+    for(int i = 0; i < rows; ++i)
+        resultsList[i] = 0;
+    printf("Successfully created results list! :D\n\n");
+}
+
+void completedProcesses(const int *resultsList, int rows) {
+    int processes = 0;
+
+    while(1){
+        for(int i = 0; i < rows; ++i)
+            if(resultsList[i] != 0)
+                processes += 1;
+
+        printf("Completed processes: %d\n", processes);
+        sleep(1);
+
+        if (processes == rows) break;
+
+        processes = 0;
+    }
+    printf("\n");
+}
+
+void printMatrix(int *matrix, int rows, int columns){
+    printf("The matrix is: \n");
+    for(int i = 0; i < rows; ++i){
+        for(int j = 0; j < columns; ++j){
+            printf("%d ", matrix[j*rows + i]);
         }
         printf("\n");
     }
-}
-
-void iniciarResultados(int *listaResultados, int filas){
-    printf("Resultados inicializados en: ");
-    for(int i=0;i<filas;i++){
-        listaResultados[i]=0;
-        printf(" %d ", listaResultados[i]);
-    }
     printf("\n");
 }
 
-void mostrarResultados(int *listaResultados, int filas){
-    printf("Los resultados son: ");
-    for(int i=0;i<filas;i++){
-        printf(" %d ", listaResultados[i]);
+void printResults(int *resultsList, int rows){
+    printf("The list of results is: \n");
+    for(int i = 0; i < rows; ++i){
+        printf("%d ", resultsList[i]);
     }
-    printf("\n");
+    printf("\n\n");
 }
 
 int main(){
+    int rows = 3, columns = 9;
 
-    // Definimos las dimensiones de la matriz
-    int filas = 3, columnas = 9;
+    // Matrix Memory
+    key_t keyMatrix = ftok(".",'J');
+    int shmIdMatrix = shmget(keyMatrix, sizeof(int)*rows*columns, 0777|IPC_CREAT);
+    int (*matrix)[columns] = shmat(shmIdMatrix, NULL, 0);
 
-    // Identificadores de memoria compartida 
-    // (midMatriz = id memoria matriz & midResultados = id memoria resultados & midProcesos == id procesos terminados)
-    int midMatriz, midResultados;
+    // Results Memory
+    key_t keyResults = ftok(".",'K');
+    int shmIdResults = shmget(keyResults, sizeof(int)*rows, 0777|IPC_CREAT);
+    int *resultsList= (int *)shmat(shmIdResults,NULL,0);
 
-    // Definimos el apuntador de la matriz, de los resultados y de los procesos terminados 
-    int *matriz, *listaResultados, procesos;
+    assignMatrixValues((int *) matrix, rows, columns);
+    assignResultsValues(resultsList, rows);
 
-    // Claves
-    key_t llaveMatriz; 
-    key_t llaveResultados; 
+    printMatrix((int *) matrix, rows, columns);
+    printResults(resultsList, rows);
 
-    // Generamos las llaves para identificar las regiones de la memoria compartida
-    llaveMatriz = ftok("Matriz",'k');
-    llaveResultados = ftok("Resultados",'k');
+    completedProcesses(resultsList, rows);
 
-     // Crea el segmento de memorias compartidas
-    midMatriz = shmget(llaveMatriz, sizeof(int)*filas*columnas, IPC_CREAT|0777); 
-    midResultados = shmget(llaveResultados, sizeof(int)*filas, IPC_CREAT|0777);
+    printResults(resultsList, rows);
 
-    // Asignando direcciones de inicios de segmentos de las memorias
-    matriz = (int *)shmat(midMatriz, NULL, 0);
-    listaResultados= (int *)shmat(midResultados,NULL,0);
+    // Free Memory
+    shmdt(matrix);
+    shmdt(resultsList);
 
-
-    // Asignando valor a los procesos terminados
-    procesos=0;
-
-    // Asignación de valores a la matriz utilizando una función
-    asignarValores(matriz, filas, columnas);
-
-    // Lectura de la matriz
-    leerMatriz(matriz, filas, columnas);
-
-    // Iniciamos los valores de los resultados 
-    iniciarResultados(listaResultados, filas);
-
-
-    int procesosTerminados = 0;
-    while(procesos == 0){
-        printf("Procesos terminados: %d\n", procesosTerminados);
-        procesosTerminados = 0;
-        for(int i=0; i<filas;i++){
-            if(listaResultados[i]!=0){
-                procesosTerminados += 1;
-            }
-        }
-        
-        if(procesosTerminados==3){
-            procesos = 1;
-        }
-        
-        sleep(2);
-    }   
-
-    mostrarResultados(listaResultados, filas);
-
-    // Liberar memoria
-    shmdt(matriz);
-    shmdt(listaResultados);
-
-    // Eliminar segmentos de memoria
-    shmctl(midMatriz,IPC_RMID,0);
-    shmctl(midResultados,IPC_RMID,0);
+    // Delete memory segments
+    shmctl(shmIdMatrix,IPC_RMID,NULL);
+    shmctl(shmIdResults,IPC_RMID,NULL);
 
     return 0;
 }
